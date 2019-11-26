daf47a3dacb0dc376226ad5bacb4527d28b4a668
Petro Moute <melie.deneuve@outlook.com>
2019-11-26 13:44:55 +0100

    css du midi page d'accueil body et footer

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,8 +1,29 @@
-<h1 id="nav">Accueil</h1>
+
 <div class="wrap">
+
+    <section id="bienvenue">
+        <h2>Bienvenue chez Carnax,<br><span>les vaccins connectés</span></h2>
+
+
+
+    <div class="section2">
+        <img src="assets/img/steto.png" alt="steto">
+        <h3>La santé c'est bien</h3>
+        <p>fhidshfisdhfihdskfhdsknhfkdsnfkdsnfkdsnkfndskkndsknfksdnfk djqdfijhqsifbhqsifb sqjfsqkjhfnkqs dsfkjhsqkhf</p>
+    </div>
+    <div class="section2">
+        <img src="assets/img/inject.png" alt="inject">
+        <h3>Les vaccins c'est choupi</h3>
+        <p>fhidshfisdhfihdskfhdsknhfkdsnfkdsnfkdsnkfndskkndsknfksdnfk djqdfijhqsifbhqsifb sqjfsqkjhfnkqs dsfkjhsqkhf</p>
+    </div>
+    </section>
+
+    <hr>
+
     <section id="equipe">
         <h2>Notre équipe</h2>
         <div class="ligne"></div>
+        <p>Blalalalala blalalala blalala balalalala bhaha ahahahahaha ahahahahaha ahahahah<br>jejejejejejejej ezheheheheh ejejejejejej</p>
 
         <div class="section1">
             <!--      <img src="assets/img/corgi.jpg" alt="Un membre de l'équipe.">-->
