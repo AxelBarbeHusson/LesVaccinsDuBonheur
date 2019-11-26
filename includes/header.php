cda7cd905951899b8aed0f232fd205e65ada668e
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-11-26 15:17:57 +0100

    affichage en fonction du role et de l'etat de connexion ok

--- includes/header.php
+++ includes/header.php
@@ -11,59 +11,61 @@ if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
 
 <header class="page-header">
     <div class="wrap">
-      <div class="log">
-        <img id="logo" src="assets/img/logov2.png" alt="logo">
-        <h4 id="titre">Carnax<br>Les vaccins connectés</h4>
-      </div>
-      <div class="icones">
-        <div class="">
-          <img src="assets/img/tel.png" alt="">
-          <p>Téléphone: 02 35 63 14 14</p>
+        <div class="log">
+            <img id="logo" src="assets/img/logov2.png" alt="logo">
+            <h4 id="titre">Carnax<br>Les vaccins connectés</h4>
         </div>
-        <div class="">
-          <img src="assets/img/iconemail.png" alt="">
-          <p>E Mail: carnax@outlook.com</p>
-        </div>
+        <div class="icones">
+            <div class="">
+                <img src="assets/img/tel.png" alt="">
+                <p>Téléphone: 02 35 63 14 14</p>
+            </div>
+            <div class="">
+                <img src="assets/img/iconemail.png" alt="">
+                <p>E Mail: carnax@outlook.com</p>
+            </div>
 
-      </div>
+        </div>
     </div>
-      <div class="clear"></div>
+    <div class="clear"></div>
 
-        <nav>
-            <ul class="nav navbar">
-                <li><a href="index.php?page=acceuil">Home</a></li>
-                <li><a href="index.php?page=rendezVous" title="">Rendez-vous</a></li>
-                <li><a href="index.php?page=etatvaccins" title="">Etat Vaccins</a></li>
-                <li><a href="index.php?page=vaccinspourvoyage" title="">Vaccins pour voyages</a></li>
-                <li><a target="_blank"
+    <nav>
+        <ul class="nav navbar">
+            <li><a href="index.php?page=acceuil">Home</a></li>
+            <li><a href="index.php?page=rendezVous" title="">Rendez-vous</a></li>
+            <li><a href="index.php?page=etatvaccins" title="">Etat Vaccins</a></li>
+            <li><a href="index.php?page=vaccinspourvoyage" title="">Vaccins pour voyages</a></li>
+            <li><a target="_blank"
                    href="https://solidarites-sante.gouv.fr/IMG/pdf/calendrier_vaccinal_maj_17avril2019.pdf">Liens
                     vaccins</a></li>
-                <li><a href="index.php?page=login">Login</a></li>
-                <li><a>Inscriptions</a></li>
-                <?php
+            <?php
+            if (!empty($_SESSION['login'])) {
 
-
-
-
-        if (ifLogged()) {
-
-            echo '<li><a>Mon compte</a></li>';
-            echo '<li class="nav-item"><a href="index.php?page=logout" class="nav-link js-scroll-trigger">Logout</a></li>';
-        } elseif (idAdmin()) {
-
+                if (ifLogged()) {
                     echo '<li><a href="index.php?page=admin" >Pannel Admin</a></li>';
+
+                    echo '<li class="nav-item"><a href="index.php?page=logout" class="nav-link js-scroll-trigger">Logout</a></li>';
+                } else {
+
+                    echo '<li><a>Mon compte</a></li>';
                     echo '<li><a href="index.php?page=logout" >Logout</a></li>';
                 }
 
-        ?>
-            </ul>
+                }else{
+                echo '<li ><a href = "index.php?page=login" > Login</a ></li >';
+                echo '<li ><a href = "index.php?page=inscriptions" > Inscriptions</a ></li >';
+            }
 
-        </nav>
+             ?>
+
+        </ul>
+
+    </nav>
 
 
     <div class="clear"></div>
 
-    <section >
+    <section>
         <img id="imagemed" src="assets/img/medimg.jpg" alt="">
 
     </section>
