38c6950830f7a934ab15b422a8e019eeff5e4616
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-11-28 15:10:33 +0100

    etat vaccins ok when you didn't at login

--- includes/etatvaccins.inc.php
+++ includes/etatvaccins.inc.php
@@ -1,1 +1,20 @@
 <h1 id="nav">Etat Vaccins</h1>
+<?php
+if (!empty($_SESSION)){
+
+}else{
+
+        $sql = "SELECT * FROM vaccins
+        WHERE 1";
+
+
+        $query = $pdo->prepare($sql);
+        $query->execute();
+        $users = $query->fetchAll();
+
+ foreach ($users as $user) {
+            echo '<div class="user">';
+            echo '- ' . $user->nom . ' crée le : ' . $user->created_at . '. Premier rappel dans (' . $user->rappel1 .') jours & second dans (' . $user->rappel2 . ') jours.';
+            echo '</div>';
+    }
+}
