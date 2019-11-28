0f0fe8230822821cf282079f0ac468ab843e2661
Thom076 <47177106+Thom076@users.noreply.github.com>
2019-11-28 10:47:56 +0100

    Ajout page RDV en Admin

---
+++ includes/rendezvousAdmin.php
@@ -0,0 +1,24 @@
+<?php
+include('includes/admin.inc.php');
+$title = 'Les rendez-vous';
+$sql   = "SELECT * FROM t_test";
+$query = $pdo->prepare($sql);
+$query->execute();
+$vaccins = $query->fetchAll();
+//debug($vaccins);
+?>
+
+<h1>Rendez-vous</h1>
+<?php foreach ($vaccins as $vaccin) { ?>
+    <div class="vaccin">
+        <h2><?php echo $vaccin['USENOM']; ['USEPRENOM'] ?></h2>
+        <p>Email :       <?= $vaccin['USEMAIL']; ?></p>
+        <p>Téléphone :   <?= $vaccin['USEPHONE']; ?></p>
+        <p>Code postal : <?= $vaccin['USECPOSTAL']; ?></p>
+        <p>Adresse :     <?= $vaccin['USEAPOSTAL']; ?></p>
+        <p>Ville :       <?= $vaccin['USEVILLE']; ?></p>
+        <p>Message :     <?= $vaccin['USEMESSAGE']; ?></p>
+        <p>Sujet :       <?= $vaccin['USESUJET']; ?></p>
+        <p>Date du Rdv : <?= $vaccin['USEDATE']; ?></p>
+    </div>
+<?php } ?>
