af091c8d0d863798e25a4e9e5261853a6861f139
Pierre <pierre.halavent@gmail.com>
2019-11-28 15:47:06 +0100

    jointure table vaccins + table t_users Affichage des vaccins propre à un utilisateur

--- includes/etatvaccins.inc.php
+++ includes/etatvaccins.inc.php
@@ -1,20 +1,35 @@
 <h1 id="nav">Etat Vaccins</h1>
 <?php
 if (!empty($_SESSION)){
+    debug($_SESSION);
+    $id = $_SESSION['login']['id'];
+    $sql = "SELECT USENOM, nom
+        FROM users_vaccins
+        JOIN t_users ON t_users.Id_Users = users_vaccins.Id_Users
+        JOIN vaccins ON vaccins.id = users_vaccins.id
+        WHERE t_users.Id_Users = $id";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $userVaccins = $query->fetchAll();
+    //debug($userVaccins);
+    echo '<div class = vaccinsUsers>';
+    echo 'Vos vaccins :';
+    foreach($userVaccins as $userVaccin) {
+        echo $userVaccin->nom;
 
+
+    }
+    echo '</div>';
 }else{
-
-        $sql = "SELECT * FROM vaccins
-        WHERE 1";
-
-
-        $query = $pdo->prepare($sql);
-        $query->execute();
-        $users = $query->fetchAll();
-
- foreach ($users as $user) {
-            echo '<div class="user">';
-            echo '- ' . $user->nom . ' crée le : ' . $user->created_at . '. Premier rappel dans (' . $user->rappel1 .') jours & second dans (' . $user->rappel2 . ') jours.';
+    $sql = "SELECT * FROM vaccins
+            WHERE 1";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $vaccins = $query->fetchAll();
+ foreach ($vaccins as $vaccin) {
+            echo '<div class="vaccins">';
+            echo '- ' . $vaccin->nom . ' crée le : ' . $vaccin->created_at . '. Premier rappel dans (' . $vaccin->rappel1 .') jours & second dans (' . $vaccin->rappel2 . ') jours.';
+            //echo $vaccin;
             echo '</div>';
     }
 }
