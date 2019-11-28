a1de7137b8bbe755f9cba5dcbe4f632d5f26d250
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-11-28 12:24:28 +0100

    supp function

--- includes/backDeleteUser.inc.php
+++ includes/backDeleteUser.inc.php
@@ -1,11 +1,13 @@
 <?php
 
-if ($_GET['login']['Id_Users'] && is_numeric($_GET['Id_Users'])){
-    $sql='SELECT * FROM t_users';
+if ($_GET['id'] && is_numeric($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "DELETE * FROM t_users
+            WHERE Id_Users = $id ";
     $query = $pdo ->prepare($sql);
     //$query -> bindValue(':id',$id,PDO::PARAM_INT);
     $query -> execute();
-    ldap_delete(localhost/carnex,$sql);
+    
     echo "Vous avez bien supprimé votre utilsateurs";
     //header("Location: manageUsers.inc.php");
 }
