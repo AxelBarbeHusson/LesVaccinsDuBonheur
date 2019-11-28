28acb65d8012929041d49915b3a504d408c61ec0
Pierre <pierre.halavent@gmail.com>
2019-11-28 11:46:46 +0100

    Ajout du mdp oublié --> l'utilisateur peut reset son mdp, ça l'update dans la bdd et ça hash le password

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -59,7 +59,7 @@ if (!isset($mdp)) $mdp = "";
     <label for="mdp"><b>Mot de passe :</b></label>
     <input type="password" placeholder="Saisir le mot de passe" id="mdp" value="<?= $mdp ?>" name="mdp"
            required>
-    <a href="index.php?page=mdpForget"><button>Mot de passe oublié ?</button></a>
+    <a href="index.php?page=mdpForget">Mot de passe oublié ?</a>
     <div>
         <input type="submit" value="Envoyer">
         <input type="reset" value="Reset">
