169ca3faf9f1483fdeabba158c417e37aa61d2e4
Pierre <pierre.halavent@gmail.com>
2019-11-28 11:59:00 +0100

    Show users panel admin

--- includes/manageUsers.inc.php
+++ includes/manageUsers.inc.php
@@ -3,7 +3,6 @@
 
 $title = 'show users';
 
-if (isset($_POST['t_users'])){
 
     $sql = "SELECT * FROM t_users
         WHERE 1";
@@ -19,7 +18,7 @@ if (isset($_POST['t_users'])){
 <?php foreach ($users as $user) {
     echo '<div class="user">';
     echo '- ' . $user->USENOM . ' ' . $user->USEPRENOM. '';
-debug($_POST);
+//debug($users);
 
     echo '<br><a href=#><span>Edit</span></a>';
     echo '<br><a href="index.php?page=backDeleteUser"><span>Delete</span></a>';
@@ -27,7 +26,6 @@ debug($_POST);
     echo '</div>';
 }
 
-}
 
 ?>
 <a href="index.php?page=addVaccins">Ajouter un nouveau vaccin</a>
