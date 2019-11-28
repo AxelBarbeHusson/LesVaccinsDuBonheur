e3ff335ee5d54ad9b76f73a263182e0dee3bcc9f
Petro Moute <melie.deneuve@outlook.com>
2019-11-28 15:11:18 +0100

    formulaires fin css

--- includes/html.php
+++ includes/html.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head class="head">
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carnax</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/403.css">
